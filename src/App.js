6c1ebe42a441657221c6d93190727616ebe8f739
salimov333 <salimov333@gmail.com>
2022-07-19 16:50:52 +0200

    Done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Timer from "./components/Timer";
 function App() {
   return (
     <div className="App">
-      <h1>React Chrono</h1>
+      <h1>React Chronometer</h1>
       <Timer />
     </div>
   );
